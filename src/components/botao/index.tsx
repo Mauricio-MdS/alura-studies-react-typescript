1145f4b7e830a7fa405982e029326f2bb14cd368
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-04-03 19:37:03 -0300

    Refat class components para function components

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{
+interface Props{
     type?: "button" | "submit" | "reset" | undefined,
     onClick?: () => void;
-}> {
-    render(){
-        const {type = 'button', onClick} = this.props;
-        return (
-            <button type={type} className={style.botao} onClick={onClick}>
-                {this.props.children}
-            </button>
-        )
-    }
+    children?: React.ReactNode;
 }
 
-export default Botao;
+export default function Botao({type, onClick, children} : Props){
+    return (
+        <button type={type} className={style.botao} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
