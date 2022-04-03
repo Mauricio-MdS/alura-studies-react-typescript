faa59d2006e59c83b748d4c4f3826aad65b7ca70
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-04-03 07:06:51 -0300

    Refatoracao da lista, componetizando os itens

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,3 +1,4 @@
+import Item from './Item';
 import style from './Lista.module.scss'
 
 function Lista(){
@@ -22,10 +23,7 @@ function Lista(){
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
+                    <Item key={index} {...item}/>
                 ) )}
             </ul>
         </aside>
