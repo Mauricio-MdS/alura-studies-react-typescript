1394f0d2460f9c50f66a233bb1bb715a3894b988
Mauricio-MdS <mauricio.martins.silva@gmail.com>
2022-04-03 08:59:47 -0300

    Associando formulario aos estados

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss'
 
 function Lista(){
 
-    const tarefas = [
+   const [tarefas, setTarefas] = useState([
         {
             tarefa: 'React',
             tempo: '02:00:00'
@@ -16,11 +17,11 @@ function Lista(){
             tarefa: 'Typescript',
             tempo: '03:00:00'
         },
-    ]
+    ]);
 
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => setTarefas([...tarefas, {tarefa: 'Next.js', tempo: '05:00:00'}])}>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item key={index} {...item}/>
